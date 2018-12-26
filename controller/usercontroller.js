1feacaefeea231918474a7bdb42b59788401f5db
jjhwang <jungting20@gmail.com>
2018-12-26 23:31:23 +0900

    일단커밋

--- controller/usercontroller.js
+++ controller/usercontroller.js
@@ -1,7 +1,7 @@
-export const join = (req,res) => res.send('user join');
-export const login = (req,res) => res.send('user login');
-export const logout = (req,res) => res.send('user logout');
-export const users = (req,res) => res.send('user users');
-export const user_detail = (req,res) => res.send('user detail');
-export const user_edit = (req,res) => res.send('user edit')
-export const change_password = (req,res) => res.send('user change password');
+export const join = (req,res) => res.render('user join');
+export const login = (req,res) => res.render('user login');
+export const logout = (req,res) => res.render('user logout');
+export const users = (req,res) => res.render('user users');
+export const user_detail = (req,res) => res.render('user detail');
+export const user_edit = (req,res) => res.render('user edit')
+export const change_password = (req,res) => res.render('user change password');
--- controller/videocontroller.js
+++ controller/videocontroller.js
@@ -1,7 +1,8 @@
-export const videohome = (req,res) => res.render("home");
-export const videosearch = (req,res) => res.send("search");
-export const videos = (req,res) => res.send('videos');
-export const delete_video = (req,res) => res.send('videos delete_video');
-export const edit_video = (req,res) => res.send('videos edit_video');
-export const videos_detail = (req,res) => res.send('videos detail');
-export const upload = (req,res) => res.send('videos upload');
+export const videohome = (req,res) => res.render("home",{ pageTitle : 'Home' });
+export const videosearch = (req,res) => res.render("search",{ pageTitle : 'Search' });
+export const videos = (req,res) => res.render('videos',{ pageTitle : 'videos' });
+export const delete_video = (req,res) => res.render('delete_video',{ pageTitle : 'delete_video' });
+export const edit_video = (req,res) => res.render('edit_video',{ pageTitle : 'edit_video' });
+export const videos_detail = (req,res) => res.render('videos_detail',{ pageTitle : 'videos_detail' });
+export const upload = (req,res) => res.render('upload',{ pageTitle : 'upload' });
+
