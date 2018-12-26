eb994fdefc892ce05b2db2727fdb82b0b68cffc4
jjhwang <jungting20@gmail.com>
2018-12-27 00:51:28 +0900

    2.19 까지 했음
    	1.git redeme.md파일을 만들어서 마크다운 살짝 맛봄
    	2.social pug 파일을 파셜에 만들어서 인클루드 했음
    	3.pug 에서 텍스트처럼보이게하는 | 이거를 배웠음

---
+++ README.md
@@ -0,0 +1,15 @@
+Cloning Youtube with Vanilla and NodeJS
+
+
+## Pages:
+
+- [ ] HOME
+- [ ] JOIN
+- [ ] Login
+- [ ] Search
+- [ ] User Detail
+- [ ] Edit Profile
+- [ ] Change Password
+- [ ] Upload
+- [ ] Video Detail
+- [ ] Edit Video
--- router/userRouter.js
+++ router/userRouter.js
@@ -2,6 +2,7 @@ import express from "express";
 import route from "../route";
 import { users, user_detail, user_edit, change_password } from "../controller/usercontroller";
 const userRouter = express.Router();
+
 userRouter.get(route.user,users);
 userRouter.get(route.user_detail,user_detail);
 userRouter.get(route.edit_profile,user_edit);
