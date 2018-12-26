991f81d170beb16243cab1449597cc9e520b0db1
jjhwang <jungting20@gmail.com>
2018-12-26 23:56:12 +0900

    render 두번째 인자로 pageTitle 넘겼음

--- controller/usercontroller.js
+++ controller/usercontroller.js
@@ -1,7 +1,7 @@
-export const join = (req,res) => res.render('user join');
-export const login = (req,res) => res.render('user login');
-export const logout = (req,res) => res.render('user logout');
-export const users = (req,res) => res.render('user users');
-export const user_detail = (req,res) => res.render('user detail');
-export const user_edit = (req,res) => res.render('user edit')
-export const change_password = (req,res) => res.render('user change password');
+export const join = (req,res) => res.render('join',{ pageTitle : ' join' });
+export const login = (req,res) => res.render('login',{ pageTitle : ' login' });
+export const logout = (req,res) => res.render('logout',{ pageTitle : ' logout' });
+export const users = (req,res) => res.render('users',{ pageTitle : ' users' });
+export const user_detail = (req,res) => res.render('user_detail',{ pageTitle : ' detail' });
+export const user_edit = (req,res) => res.render('edit_profile',{ pageTitle : 'edit_profile' })
+export const change_password = (req,res) => res.render('change_password',{ pageTitle : ' change password' });
--- router/videoRouter.js
+++ router/videoRouter.js
@@ -2,7 +2,7 @@ import express from "express";
 import route from "../route";
 import { videos, delete_video, edit_video, videos_detail, upload } from "../controller/videocontroller";
 const videoRouter = express.Router();
-
+    
 videoRouter.get(route.videos,videos);
 videoRouter.get(route.delete_video,delete_video);
 videoRouter.get(route.edit_video,edit_video);
